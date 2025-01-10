4062c02ca91e6c55df41df1f4bed15e807f34454
Michał Basiński <michal.basinski88@gmail.com>
2025-01-10 11:49:10 +0100

    Init Spring Boot project

---
+++ src/main/kotlin/org/miniatures/collection/manager/MiniaturesCollectionManagerApplication.kt
@@ -0,0 +1,11 @@
+package org.miniatures.collection.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniaturesCollectionManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniaturesCollectionManagerApplication>(*args)
+}
